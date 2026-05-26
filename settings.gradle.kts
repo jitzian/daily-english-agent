14db2299d93481b77fd784a7c83304f2f9fc322b
joNA <jitzian@gmail.com>
2026-05-25 20:34:11 -0400

    [ai agent] js - Fixing dependencies - Updating coroutines - Adding 'mavenCentral()'

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
 	repositories {
-		maven { url = uri("https://repo.spring.io/snapshot") }
 		gradlePluginPortal()
+		mavenCentral()
 	}
 }
 rootProject.name = "daily-english-agent"
